86e914372ba409f0f596edfdde1008bac5287ce4
Justin Munn <contact@justinmunn.co>
2017-01-29 00:40:41 +0000

    Associated people with places, updated sign in and sign up forms

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 3)
   end
@@ -9,11 +9,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
   private
+
   def place_params
     params.require(:place).permit(:name, :address, :description)
   end
