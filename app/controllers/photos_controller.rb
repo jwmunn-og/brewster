b6b0d2dc9ddeb39dc4f50ec21e1fb5dc5b38e86b
Justin Munn <contact@justinmunn.co>
2017-02-12 14:19:55 -0800

    Added AWS region params to fix default conflict.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,7 +4,6 @@ class PhotosController < ApplicationController
   def create
     @place = Place.find(params[:place_id])
     @place.photos.create(photo_params)
-    # render 'places/show', status: :unprocessable_entity
     redirect_to place_path(@place)
   end
 
