43c480ae1f0e927c8dca7f709a77e83cf5b8bc1a
jwmunn <contact@justinmunn.co>
2017-02-12 22:04:40 -0800

    Successfully sent test email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
     default from: 'no-reply@nomsterapp.com'
 
     def comment_added
-        mail(to: "justinwmunn@gmail.com",
+        mail(to: "justinmunn2@gmail.com",
          subject: "A comment has been added to your place")
     end
 end
